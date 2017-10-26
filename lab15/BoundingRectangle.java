8829c3782bb2168445802abbae1ef2a0d86a7dc5
songdavid98 <songdavid98@gmail.com>
2017-10-26 17:15:18 -0400

    lab15

---
+++ lab15/BoundingRectangle.java
@@ -0,0 +1,53 @@
+import javafx.application.Application; 
+import javafx.stage.Stage; 
+import javafx.scene.Scene; 
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Circle;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
+public class BoundingRectangle extends Application{
+
+	public void start(Stage primaryStage) {
+		Pane p = new Pane();
+		Rectangle r = new Rectangle(100,100,10,10);
+		r.setFill(Color.WHITE);
+		r.setStroke(Color.BLUE);
+		p.getChildren().add(r);
+
+		p.setOnMouseClicked( e -> {
+			Circle circle = new Circle( e.getX(), e.getY(), 10);
+			circle.setOnMouseClicked( c -> {
+				c.consume();
+				p.getChildren().remove(circle);
+			});
+			p.getChildren().add(circle);
+
+			if (e.getX() < r.getX()) {
+				r.setWidth( r.getWidth() + r.getX() - e.getX() + 10);
+				r.setX( e.getX() -10 );
+			}
+			else if (e.getX() > r.getX() + r.getWidth()) {
+				r.setWidth( e.getX() - r.getX() + 10);
+			}
+			if (e.getY() < r.getY()) {
+				r.setHeight( r.getHeight() + r.getY() - e.getY() + 10);
+				r.setY( e.getY() -10 );
+			}
+			else if (e.getY() > r.getY() + r.getHeight()) {
+				r.setHeight( e.getY() - r.getY() + 10);
+			}
+
+		});
+
+		Scene scene = new Scene( p , 200, 200);
+
+		primaryStage.setTitle("MyJavaFX");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
---
+++ lab15/CircleGame.java
@@ -0,0 +1,50 @@
+import javafx.application.Application; 
+import javafx.stage.Stage; 
+import javafx.scene.Scene; 
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Circle;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+import java.util.Random;
+//import java.util.Date;
+
+public class CircleGame extends Application {
+
+	int counter = 0;
+	double now = (double)System.currentTimeMillis()/1000;
+
+	public void start(Stage primaryStage) {
+		Pane p = new Pane();
+		int sw = 200;
+		int sh = 200;
+
+		Scene scene = new Scene( p , sw, sh);
+
+		Random r = new Random();
+		Circle c = new Circle(r.nextInt(sw-20) + 10,  r.nextInt(sh-20) + 10, 10);
+		c.setFill( Color.color(Math.random(), Math.random(), Math.random() ) );
+
+
+		
+		c.setOnMouseClicked( e -> {
+				counter++;
+				if (counter >= 5) {
+					now = (double)System.currentTimeMillis()/1000 - now;
+					Text t = new Text(50,100, Double.toString(now));
+					p.getChildren().add(t);
+					p.getChildren().remove(c);
+				}
+				c.setCenterX(r.nextInt(sw-20) + 10);
+				c.setCenterY(r.nextInt(sh-20) + 10);
+				c.setFill( Color.color(Math.random(), Math.random(), Math.random()) );
+			});
+		p.getChildren().add(c);
+		primaryStage.setTitle("MyJavaFX");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
